d17e01b9a0a07ec2666de51049c8d085c43cbe9b
vladislavi4ek <rusguba@mail.ru>
2021-09-05 20:20:53 +0300

    Created the Lesson-4 branch. Completed homework. I checked the item 5 using Jest.

--- main.js
+++ main.js
@@ -29,3 +29,86 @@ for (let i=0; i < arr2.length; i++)
         console.log(arr2[i]);
     }
   }
+
+  function isPalindrome(str) {
+    let strLen = str.length, strReverse = str.split ('') .reverse( ) .join('');
+    // Разбила строку посимвольно и превратила в массив с помощью split ( '' ), применила метод reverse( ) и собрала все в символьную строку с помощью join( '' )
+    if (strReverse == str) {
+        return Boolean(strReverse == str);
+    } else {
+        return Boolean(strReverse == str);
+    }
+  }
+  let palindrome = isPalindrome ('lol');
+  let palindrome2 = isPalindrome ('marta');
+  console.log (palindrome);
+  console.log (palindrome2);
+  
+  function min (a, b) {
+      if (a < b) {
+          return a;
+      } else {
+          return b;
+      }
+  }
+  
+  function max (a, b) {
+      if (a > b) {
+          return a;
+      } else {
+          return b;
+      }
+  }
+  
+  //Функции через тернарный оператор
+  function min (a, b) {
+      return a < b ? a : b;
+  }
+  
+  function max (a, b) {
+      return a > b ? a : b;
+  }
+  
+  function changeZero (number) {
+  const numStr = `${number}`; // нужно писать `${number}` именно в таком виде, чтобы замена вставилась на его место
+  if (numStr.includes('0')){ // содержит ли элемент numStr 0 определяется через .includes('0')
+  return numStr.replaceAll('0', 'zero'); // заменяет все 0 на zero через .replaceAll
+  }
+  return number;
+  }
+  console.log ([80, 1, 2, 500] .map(changeZero).join(' ')); // .map позволило создать новый массив
+  
+
+
+
+
+  function sum(a,b){
+    if(b===undefined){
+        return function(c){ return parseInt(a)+parseInt(c);} //  Функция parseInt преобразует первый переданный ей аргумент в строковый тип, интерпретирует его и возвращает целое число или значение NaN
+    }
+    return parseInt(a)+parseInt(b);
+}
+console.log(sum(5)(2)); // 5
+
+// module.exports = sum;
+
+
+
+var colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+var p_collect = document.getElementsByTagName('p');
+
+var f1 = function() {
+	var k = 0;
+	return function() {
+		this.style.color = colors[k];
+		k++;
+		if (k == colors.length){k=0};
+	}
+}
+
+for (var i = 0; i < p_collect.length; i++) {
+	p_collect[i].addEventListener('click', f1());
+}
+
+
+  
