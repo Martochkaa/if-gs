6daa50fc4a94e7d2699f784e58d80a7873660ef3
vladislavi4ek <rusguba@mail.ru>
2021-09-21 21:50:55 +0300

    The lesson-7 branch has been created. Homework completed.

--- main.js
+++ main.js
@@ -701,3 +701,59 @@ for  (let i=0; i<arrcountry.length; i++) { // для полученного ма
 return resultarr;
 }
 console.log(СountriesAndCities(Exercise7)) // вместо аргумента х записываем нужный нам массив
+
+
+
+
+////////lesson-7
+const obj1 = {
+    a: 'a',
+    b: {
+      a: 'a',
+      b: 'b',
+      c: {
+        a: 1,
+      },
+    },
+ };
+ const obj2 = {
+    b: {
+      c: {
+        a: 1,
+      },
+      b: 'b',
+      a: 'a',
+    },
+    a: 'a',
+ };
+ const obj3 = {
+    a: {
+      c: {
+        a: 'a',
+      },
+      b: 'b',
+      a: 'a',
+    },
+    b: 'b',
+ };
+
+ function deepEqual(obj1, obj2) { // Цикл через свойства объекта obj1
+    for (var p in obj1) { //Проверка на то, что оба объекта существуют
+      if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+      switch (typeof (obj1[p])) {  // Глубокое сравнение объектов по ключам и значения:
+        case 'object':
+          if (!deepEqual(obj1[p], obj2[p])) return false;
+          break; 
+          // Сравнение данных типа function:
+        case 'function':
+          if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+          break;  // Сравнение значений:
+        default:
+          if (obj1[p] != obj2[p]) return false;
+      }
+    }
+    return true;
+  }
+
+ console.log (deepEqual(obj1, obj2)); // true
+ console.log (deepEqual(obj1, obj3)); // false
