ec2bc154b4fed78a76661c9989bead184398fdc0
Kaleeva <martik1000mal.ru@mail.ru>
2021-10-10 17:39:45 +0300

    Help find bugs or rewrite the code to make it work.

---
+++ lesson-10/lesson10.js
@@ -0,0 +1,67 @@
+
+const data = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa  Cruz de Tenerife',
+    country: 'Spain',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
+  },
+];
+
+let currentIndex = 0;
+
+data.forEach(element => {
+console.log(element)
+currentIndex++
+});
+
+
+const slide = document.getElementById("slider");
+const btnRight = document.getElementById("btn-right");
+const names = document.getElementById("name").innerHTML = `<p id="name">${data.name}</p>`;;
+const hotelimg = document.getElementById("hotel_img").innerHTML = `<img class="hotel_img" id="hotel_img" src="${data.imageUrl}"`;;
+const country = document.getElementById("text_hotel_leopold").innerHTML = `<p class="text_hotel_leopold" id="text_hotel_leopold">${data.country}</p>`;
+const city = document.getElementById ("location_leopold").innerHTML = `<p class="location_leopold" id="location_leopold">${data.city}</p>`;
+
