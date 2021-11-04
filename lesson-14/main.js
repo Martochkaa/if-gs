611a4dff37270004d5b16612d2322d416b1aec54
unknown <you@example.com>
2021-11-04 19:38:54 +0300

    bugs fixed

--- lesson-14/main.js
+++ lesson-14/main.js
@@ -6,13 +6,13 @@ formEl.addEventListener('submit', async event => {
         method: 'Post',
         body: new FormData(formEl),
     };
-    const result = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOption).then(Response => {
-        if (!Response.ok) {
-            throw new Error(Response.statusText);
+    const result = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOption).then(response => {
+        if (!response.ok) {
+            throw new Error(response.statusText);
         }
-        return Response.json();
+        return response.json();
     })
     .then(result => result)
     .catch(error => console.log(error.message));
-    console.log(result)
+    console.log(result);
 })
